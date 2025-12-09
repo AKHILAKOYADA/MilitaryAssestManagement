48b057a93e22dab5fe88acfb122951c896969195
divi84 <divijasri2122@gmail.com>
2025-12-09 10:07:50 +0530

    Fix: Bind server to 0.0.0.0 for Render health checks

--- server/server.js
+++ server/server.js
@@ -229,6 +229,6 @@ app.get('/api/transactions', authenticateJWT, (req, res) => {
 });
 
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
